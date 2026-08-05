9a90c42f15ac684dc1d0f68a4bfd4f2c6b63f1cd
peibinliang <1805993643@qq.com>
2026-08-05 11:58:53 +0800

    test: reproduce paste and shortcut regressions

--- MacPasteHistoryTests/BrandAndInteractionTests.swift
+++ MacPasteHistoryTests/BrandAndInteractionTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import AppKit
 @testable import MacPasteHistory
 
 final class BrandAndInteractionTests: XCTestCase {
@@ -9,4 +10,55 @@ final class BrandAndInteractionTests: XCTestCase {
     func testHistoryRowInteraction_whenSingleClicked_shouldPasteImmediately() {
         XCTAssertEqual(HistoryRowInteraction.primaryAction, .paste)
     }
+
+    func testHistoryPanelAppearance_shouldSuppressDefaultKeyboardFocusRing() {
+        XCTAssertTrue(HistoryPanelWindow.keyboardFocusEffectDisabled)
+    }
+
+    func testPasteActivationPolicy_shouldForcePreviousApplicationToFront() {
+        XCTAssertTrue(PasteActivationPolicy.options.contains(.activateIgnoringOtherApps))
+    }
+
+    func testPasteTargetPolicy_whenZhanYiIsFrontmost_shouldKeepLastExternalApplication() {
+        let targetBundleIdentifier = PasteTargetPolicy.preferredBundleIdentifier(
+            frontmostBundleIdentifier: "com.peibin.MacPasteHistory",
+            lastExternalBundleIdentifier: "com.apple.TextEdit",
+            ownBundleIdentifier: "com.peibin.MacPasteHistory"
+        )
+
+        XCTAssertEqual(targetBundleIdentifier, "com.apple.TextEdit")
+    }
+
+    func testPasteTargetPolicy_whenExternalApplicationIsFrontmost_shouldPreferIt() {
+        let targetBundleIdentifier = PasteTargetPolicy.preferredBundleIdentifier(
+            frontmostBundleIdentifier: "com.apple.Safari",
+            lastExternalBundleIdentifier: "com.apple.TextEdit",
+            ownBundleIdentifier: "com.peibin.MacPasteHistory"
+        )
+
+        XCTAssertEqual(targetBundleIdentifier, "com.apple.Safari")
+    }
+
+    @MainActor
+    func testAppDelegateSettingsFactory_shouldReuseApplicationShortcutService() {
+        let defaults = UserDefaults(suiteName: "BrandAndInteractionTests.\(UUID().uuidString)")!
+        let config = UserDefaultsConfig(defaults: defaults)
+        let shortcutService = ShortcutService(
+            config: config,
+            registrationManager: BrandFakeShortcutRegistrationManager()
+        )
+        let appDelegate = AppDelegate(shortcutService: shortcutService)
+
+        let viewModel = appDelegate.makeSettingsViewModel(config: config)
+
+        XCTAssertTrue(viewModel.shortcutService === shortcutService)
+    }
+}
+
+private final class BrandFakeShortcutRegistrationManager: ShortcutRegistrationManaging {
+    func register(keyCode: UInt32, modifiers: UInt32) -> OSStatus {
+        noErr
+    }
+
+    func unregister() {}
 }
