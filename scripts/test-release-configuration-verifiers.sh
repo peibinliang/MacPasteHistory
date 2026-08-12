b82624c0246af19de6ce4bc6faedc7af4f2a28c1
peibinliang <1805993643@qq.com>
2026-08-12 11:36:02 +0800

    test: align release verifier fixture with v1.0.3

--- scripts/test-release-configuration-verifiers.sh
+++ scripts/test-release-configuration-verifiers.sh
@@ -134,7 +134,7 @@ expect_failure \
     "$CURRENT_FIXTURE/scripts/verify-release-entitlements.sh"
 
 create_fixture
-perl -pi -e 's/1\.0\.2/1x0x2/g' \
+perl -pi -e 's/1\.0\.3/1x0x3/g' \
     "$CURRENT_FIXTURE/docs/release/RELEASE_PREP_GUIDE.md" \
     "$CURRENT_FIXTURE/docs/release/manual-qa-record.md"
 expect_failure \
